330e4cd28a5c578ed22aab9ed1d7157426141302
Сергей Горбатенко <gorbatenkosy@ema.ru>
2025-11-07 20:48:54 +0700

    Some cleanup

--- deps.py
+++ deps.py
@@ -13,8 +13,7 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
 def get_current_user(
     db: Session = Depends(get_db),
     token: str = Depends(oauth2_scheme)
-) -> User: # <-- Рекомендуется добавить type hint
-    # ... без изменений
+) -> User: 
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
